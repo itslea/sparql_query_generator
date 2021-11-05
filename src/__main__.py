22d75bc7a2832247bd987df68a3aacc5cb9683ca
Lea <->
2021-11-05 18:25:05 +0100

    modified __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,6 @@
 import requests
 
-query_object = "SELECT DISTINCT ?o WHERE{?s ?p ?o.} LIMIT 10"  # noqa: E501
+query_object = "SELECT DISTINCT *  FROM <http://dbpedia.org> WHERE{?s ?p ?o.} LIMIT 100"  # noqa: E501
 req_object = requests.get('http://localhost:8890/sparql', params={'format': 'json', 'query': query_object})
 resultobj = req_object.json()
 
