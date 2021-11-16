1c169d2d186876a93bfc2e9ba500574264c4a177
Lea <->
2021-11-16 19:08:53 +0100

    added create_query_shape() in star_subject_generator.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,4 @@
 # from generators.star_subject_generator import *
-import json
 import generators.star_subject_generator as ssg
 
-result = ssg.generate_query(4, 4, 0.7, 0.5)
-print(result['results']['bindings'])
+result = ssg.generate_query(4, 5, 0.7, 0.5)
--- src/generators/star_subject_generator.py
+++ src/generators/star_subject_generator.py
@@ -1,3 +1,4 @@
+import random
 import requests
 
 
@@ -20,16 +21,60 @@ def fetch_data(triples):
     filter_string += ')'
     fetch_query += filter_string + ' FILTER(1 > <SHORT_OR_LONG::bif:rnd> (10000, ?s)) } LIMIT 1'  # TODO: takes too long to process ' } ORDER BY RAND() LIMIT 1', choose random answer
 
-    print(fetch_query)
-    result = requests.get('http://localhost:8890/sparql', params={'format': 'json', 'query': fetch_query})
+    # print(fetch_query)
+    result = requests.get('https://dbpedia.org/sparql', params={'format': 'json', 'query': fetch_query})
     return result.json()
+
+
+def create_query_shape(endpoint_data, triples, var_prob):
+    """Creates the basic shape of the query while replacing constants with
+    variables according to the variable probability"""
+
+    endpoint_data = endpoint_data['results']['bindings'][0]
+    # print(endpoint_data)
+
+    query_shape = ''
+    pred_var_counter = 1
+    obj_var_counter = 1
+
+    subject = endpoint_data['s']
+    if random.random() <= var_prob:
+        subject = '?s'
+    else:
+        if subject['type'] == 'uri':
+            subject = '<' + subject['value'] + '>'
+        # elif(subject['type' == ]) blank node
+
+    for i in range(1, triples + 1):
+        predicate = endpoint_data['p' + str(i)]
+        objectt = endpoint_data['o' + str(i)]
+
+        if random.random() <= var_prob:
+            predicate = '?p' + str(pred_var_counter)
+            pred_var_counter += 1
+        else:
+            predicate = '<' + predicate['value'] + '>'
+
+        if random.random() <= var_prob:
+            objectt = '?o' + str(obj_var_counter)
+            obj_var_counter += 1
+        else:
+            if objectt['type'] == 'uri':
+                objectt = '<' + objectt['value'] + '>'
+            elif objectt['type'] == 'literal':
+                objectt = '\"' + objectt['value'] + '\"'
+            # elif blank node
+
+        query_shape += subject + ' ' + predicate + ' ' + objectt + ' . '
+
+    return query_shape
 
 
 def generate_query(queries, triples, operator_prob, var_prob):
     """Generates query."""
-
     query = ''
-    
     endpoint_data = fetch_data(triples)
-    return(endpoint_data)
+    query_shape = create_query_shape(endpoint_data, triples, var_prob)
+    print(query_shape)
 
+    return endpoint_data
